1522844b8a271403c854dff91c89505cf8a20c5a
Aswin Shine <ashwinsh.91@gmail.com>
2025-11-30 15:48:20 +0530

    Update terraform.tf

--- terraform-eks/terraform.tf
+++ terraform-eks/terraform.tf
@@ -4,9 +4,39 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.22.1"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14"
+    }
   }
 }
 
 provider "aws" {
   region = var.aws_region
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.wanderlust_eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.wanderlust_eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.wanderlust_eks.cluster_name]
+    }
+  }
+}
+
+provider "kubectl" {
+  host                   = module.wanderlust_eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.wanderlust_eks.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.wanderlust_eks.cluster_name]
+  }
 }
